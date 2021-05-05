fe3231b2ef44c6ccbea41d9dd7d5eee4874c1b8d
nids4easy <nids4easy@gmail.com>
2021-05-06 05:42:13 +0700

    main

---
+++ scala_start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --algorithm panthera --pool sg.scala.herominers.com:10130 --wallet SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb --password oncom --cpu-threads 1 --disable-gpu

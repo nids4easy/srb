4ba87d2f9f7efbc4f0fc560f517749bd9eac1c9a
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-06 05:24:41 +0700

    Create scala_start.sh

---
+++ scala_start.sh
@@ -0,0 +1,1 @@
+./SRBMiner-MULTI --algorithm panthera --pool sg.scala.herominers.com:10130 --wallet SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb --password oncom --cpu-threads 1 --disable-gpu
